d1ffcdfd87d9873e36a22910e9be2494e88d7d63
Alysson <alysson6801@hotmail.com>
2021-12-14 10:05:16 -0300

    chore: fisish the type of buy

--- src/Components/NewTransactionModal/styles.ts
+++ src/Components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
 
@@ -60,6 +60,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -67,7 +73,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${({ isActive }) => isActive ? '#eee' : 'transparent'};
+  background: ${({ isActive, activeColor }) => isActive ? 
+    transparentize(0.9, colors[activeColor]) : 
+    'transparent'};
 
   display: flex;
   align-items: center;
