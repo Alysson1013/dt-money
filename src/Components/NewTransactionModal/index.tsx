c439df85d492ad4f48430c11e3dffabc85a375ae
Alysson <alysson6801@hotmail.com>
2021-12-14 09:42:09 -0300

    chore: style transaction button type

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from "./styles";
 
 
 interface NewTransactionModalProps {
@@ -20,30 +23,45 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button 
-        type="button" 
+      <button
+        type="button"
         onClick={onRequestClose}
         className="react-modal-close"
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
-    
+
       <Container>
         <h2> Cadastrar transação </h2>
 
-        <input 
+        <input
           type="text"
           placeholder="Título"
         />
-      
-        <input 
+
+        <input
           type="number"
           placeholder="Valor"
         />
 
-        <input 
-          type="text" 
-          placeholder="Categoria"  
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>
+              Entrada
+            </span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>
+              Saída
+            </span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder="Categoria"
         />
 
         <button type="submit">
